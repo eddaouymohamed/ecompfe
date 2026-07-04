94ebdaa2ec64b435607849d19b3ca341e6c25314
Mohamed Ed-daouy <eddaouymohamed1234@gmail.com>
2026-07-04 17:49:22 +0200

    second render backend separation

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,3 @@
-
 import cors from 'cors';
 
 import express from 'express';
@@ -6,7 +5,6 @@ import errorHandlerMidlleware from './middleware/error.js'
 import products from './routes/productsRoutes.js';
 import user from "./routes/userRoutes.js"
 import order from './routes/orderRoutes.js';
-// import payment from './routes/paymentRoutes.js';
 import payment from './routes/paymentRoutes.js';
 import cookieParser from 'cookie-parser';
 import fileUpload from 'express-fileupload';
@@ -14,35 +12,50 @@ import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-
 if(process.env.NODE_ENV!=='PRODUCTION'){
 dotenv.config({ path: 'backend/config/config.env' })
 }
+
 const __filename=fileURLToPath(import.meta.url);
 const __dirname=path.dirname(__filename);
 
 const app = express()
-app.use(express.json({ limit: '30mb' })) //pase jspn dtata into object
+
+app.use(express.json({ limit: '30mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }))
 app.use(cookieParser());
-app.use(fileUpload())
+app.use(fileUpload());
 
-//route
+/* =========================
+   ROUTES
+========================= */
 app.use('/api/v1', products)
 app.use('/api/v1', user)
 app.use('/api/v1', order)
 app.use('/api/v1', payment)
 
+/* =========================
+   CORS
+========================= */
 app.use(cors({
-  origin: "http://localhost:5173", // ton frontend
+  origin: "http://localhost:5173", // ton frontend (DEV)
   credentials: true
 }));
-//serve static files
+
+/* =========================
+   STATIC FRONTEND (COMMENTÉ POUR RENDER)
+   👉 IMPORTANT: à activer seulement si backend + frontend ensemble
+========================= */
+
 // app.use(express.static(path.join(__dirname,'../frontend/dist')));
+
 // app.get(/^\/(?!api\/).*/, (req, res) => {
 //   res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
 // });
 
+/* =========================
+   ERROR HANDLER
+========================= */
 app.use(errorHandlerMidlleware)
 
 export default app;
